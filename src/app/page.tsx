0c08131db660012f3b053e4efba4557a266275d5
wen527555 <wen527555@gmail.com>
2024-09-23 01:39:40 +0800

    feat(homepage): display articles and enable navigation to article view page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <main></main>;
+import ArticleList from '@/components/AllArticle';
+
+// import { fetchAllPublishedArticles } from '@/lib/firebaseApi';
+
+export default async function Home() {
+  // const articles = await fetchAllPublishedArticles();
+  // console.log(' articles', articles);
+  return (
+    <>
+      <ArticleList />
+    </>
+  );
 }

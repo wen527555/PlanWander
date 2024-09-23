3c37617e32f5ef91c5baf917b798ae0d2569074d
wen527555 <wen527555@gmail.com>
2024-09-23 11:27:47 +0800

    fix(map): fix style is not done loading error

--- src/app/articles/[articleId]/Map.tsx
+++ src/app/articles/[articleId]/Map.tsx
@@ -34,24 +34,46 @@ const MapComponent: React.FC<MapComponentProps> = ({ places = [], routes = [], v
     zoom: 1.5,
   });
 
+  const handleMapLoad = () => {
+    const map = mapRef.current;
+    if (map && map.isStyleLoaded()) {
+      setIsMapLoaded(true);
+    }
+  };
+
+  const handleStyleLoad = () => {
+    setIsMapLoaded(true);
+  };
+
+  useEffect(() => {
+    const map = mapRef.current;
+    if (map) {
+      if (map.isStyleLoaded()) {
+        setIsMapLoaded(true);
+      } else {
+        map.on('style.load', handleStyleLoad);
+        return () => {
+          map.off('style.load', handleStyleLoad);
+        };
+      }
+    }
+  }, [mapRef]);
+
   useEffect(() => {
     if (mapRef.current && isMapLoaded && places.length > 0) {
-      const checkIfStyleLoaded = () => {
-        if (mapRef.current.isStyleLoaded()) {
-          const bounds = new LngLatBounds();
-          places.forEach((place) => bounds.extend([place.lng, place.lat]));
-          mapRef.current.fitBounds(bounds, {
-            padding: 50,
-            maxZoom: 15,
-          });
-        } else {
-          setTimeout(checkIfStyleLoaded, 1000);
-        }
-      };
-
-      checkIfStyleLoaded();
+      const bounds = new LngLatBounds();
+      places.forEach((place) => bounds.extend([place.lng, place.lat]));
+      mapRef.current.fitBounds(bounds, {
+        padding: 50,
+        maxZoom: 15,
+      });
     }
   }, [places, isMapLoaded]);
+
+  const handleViewStateChange = (evt: ViewStateChangeEvent) => {
+    setViewState(evt.viewState);
+  };
+
   //   console.log('visiblePlace', visiblePlace);
   useEffect(() => {
     if (visiblePlace && isMapLoaded && mapRef.current) {
@@ -68,14 +90,6 @@ const MapComponent: React.FC<MapComponentProps> = ({ places = [], routes = [], v
     }
   }, [visiblePlace, places, isMapLoaded]);
 
-  const handleMapLoad = () => {
-    setIsMapLoaded(true);
-  };
-
-  const handleViewStateChange = (evt: ViewStateChangeEvent) => {
-    setViewState(evt.viewState);
-  };
-
   return (
     <div style={{ width: '100%', height: '100vh' }}>
       <Map
@@ -87,55 +101,57 @@ const MapComponent: React.FC<MapComponentProps> = ({ places = [], routes = [], v
         onLoad={handleMapLoad}
         ref={mapRef}
       >
-        {places?.map((place, index) => (
-          <Marker
-            key={index}
-            longitude={place.lng}
-            latitude={place.lat}
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: `${place.color}`,
-              //   backgroundColor: visiblePlace === place.id ? '#ff0000' : `${place.color}`, // 放大時變為紅色
-              width: visiblePlace === place.id ? '50px' : '32px',
-              height: visiblePlace === place.id ? '50px' : '32px',
-              borderRadius: '50%',
-              border: `2px solid #ffff`,
-              //   width: '32px',
-              //   height: '32px',
-              color: 'white',
-              fontWeight: 'bold',
-              fontSize: '15px',
-            }}
-          >
-            {place.number}
-          </Marker>
-        ))}
-        {routes?.map((route, index) => (
-          <Source
-            key={`route-${index}`}
-            id={`route-${index}`}
-            type="geojson"
-            data={{
-              type: 'Feature',
-              geometry: {
-                type: route.type,
-                coordinates: route.coordinates,
-              },
-            }}
-          >
-            <Layer
+        {isMapLoaded &&
+          places?.map((place, index) => (
+            <Marker
+              key={index}
+              longitude={place.lng}
+              latitude={place.lat}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: `${place.color}`,
+                //   backgroundColor: visiblePlace === place.id ? '#ff0000' : `${place.color}`, // 放大時變為紅色
+                width: visiblePlace === place.id ? '50px' : '32px',
+                height: visiblePlace === place.id ? '50px' : '32px',
+                borderRadius: '50%',
+                border: `2px solid #ffff`,
+                //   width: '32px',
+                //   height: '32px',
+                color: 'white',
+                fontWeight: 'bold',
+                fontSize: '15px',
+              }}
+            >
+              {place.number}
+            </Marker>
+          ))}
+        {isMapLoaded &&
+          routes?.map((route, index) => (
+            <Source
               key={`route-${index}`}
               id={`route-${index}`}
-              type="line"
-              paint={{
-                'line-color': route.color,
-                'line-width': 5,
+              type="geojson"
+              data={{
+                type: 'Feature',
+                geometry: {
+                  type: route.type,
+                  coordinates: route.coordinates,
+                },
               }}
-            />
-          </Source>
-        ))}
+            >
+              <Layer
+                key={`route-${index}`}
+                id={`route-${index}`}
+                type="line"
+                paint={{
+                  'line-color': route.color,
+                  'line-width': 5,
+                }}
+              />
+            </Source>
+          ))}
       </Map>
     </div>
   );
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -36,28 +36,41 @@ const MapComponent: React.FC<MapComponentProps> = ({ places = [], routes = [], o
     zoom: 1.5,
   });
 
-  useEffect(() => {
-    if (mapRef.current && isMapLoaded && places.length > 0) {
-      const checkIfStyleLoaded = () => {
-        if (mapRef.current.isStyleLoaded()) {
-          const bounds = new LngLatBounds();
-          places.forEach((place) => bounds.extend([place.lng, place.lat]));
-          mapRef.current.fitBounds(bounds, {
-            padding: 50,
-            maxZoom: 15,
-          });
-        } else {
-          setTimeout(checkIfStyleLoaded, 1000);
-        }
-      };
-
-      checkIfStyleLoaded();
-    }
-  }, [places, isMapLoaded]);
-
   const handleMapLoad = () => {
+    const map = mapRef.current;
+    if (map && map.isStyleLoaded()) {
+      setIsMapLoaded(true);
+    }
+  };
+
+  const handleStyleLoad = () => {
     setIsMapLoaded(true);
   };
+
+  useEffect(() => {
+    const map = mapRef.current;
+    if (map) {
+      if (map.isStyleLoaded()) {
+        setIsMapLoaded(true);
+      } else {
+        map.on('style.load', handleStyleLoad);
+        return () => {
+          map.off('style.load', handleStyleLoad);
+        };
+      }
+    }
+  }, [mapRef]);
+
+  useEffect(() => {
+    if (mapRef.current && isMapLoaded && places.length > 0) {
+      const bounds = new LngLatBounds();
+      places.forEach((place) => bounds.extend([place.lng, place.lat]));
+      mapRef.current.fitBounds(bounds, {
+        padding: 50,
+        maxZoom: 15,
+      });
+    }
+  }, [places, isMapLoaded]);
 
   const handleViewStateChange = (evt: ViewStateChangeEvent) => {
     setViewState(evt.viewState);
@@ -87,68 +100,59 @@ const MapComponent: React.FC<MapComponentProps> = ({ places = [], routes = [], o
         onLoad={handleMapLoad}
         ref={mapRef}
       >
-        {places?.map((place, index) => (
-          <Marker
-            key={index}
-            longitude={place.lng}
-            latitude={place.lat}
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: `${place.color}`,
-              borderRadius: '50%',
-              border: `2px solid #ffff`,
-              width: '32px',
-              height: '32px',
-              color: 'white',
-              fontWeight: 'bold',
-              fontSize: '15px',
-            }}
-            onClick={() => handleMarkerClick(place)}
-          >
-            {place.number}
-          </Marker>
-        ))}
-        {/* {selectedPlace && (
-          <Popup
-            longitude={selectedPlace.lng}
-            latitude={selectedPlace.lat}
-            onClose={() => setSelectedPlace(null)}
-            closeOnClick={false}
-          >
-            <div>
-              <h3>{selectedPlace.name}</h3>
-            </div>
-          </Popup>
-        )} */}
-        {/* <RouteLayer places={places} /> */}
-        {routes?.map((route, index) => (
-          <Source
-            key={`route-${index}`}
-            id={`route-${index}`}
-            type="geojson"
-            data={{
-              type: 'Feature',
-              geometry: {
-                type: route.type,
-                coordinates: route.coordinates,
-              },
-            }}
-          >
-            <Layer
+        {isMapLoaded &&
+          places?.map((place, index) => (
+            <Marker
+              key={index}
+              longitude={place.lng}
+              latitude={place.lat}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: `${place.color}`,
+                borderRadius: '50%',
+                border: `2px solid #ffff`,
+                width: '32px',
+                height: '32px',
+                color: 'white',
+                fontWeight: 'bold',
+                fontSize: '15px',
+                cursor: 'pointer',
+              }}
+              onClick={() => handleMarkerClick(place)}
+            >
+              {place.number}
+            </Marker>
+          ))}
+
+        {isMapLoaded &&
+          routes?.map((route, index) => (
+            <Source
               key={`route-${index}`}
               id={`route-${index}`}
-              type="line"
-              paint={{
-                'line-color': route.color,
-                'line-width': 5,
-                //   'line-opacity': 0.9,
-                // 'line-dasharray': [3, 3],
+              type="geojson"
+              data={{
+                type: 'Feature',
+                geometry: {
+                  type: route.type,
+                  coordinates: route.coordinates,
+                },
               }}
-            />
-          </Source>
-        ))}
+            >
+              <Layer
+                key={`route-${index}`}
+                id={`route-${index}`}
+                type="line"
+                paint={{
+                  'line-color': route.color,
+                  'line-width': 5,
+                  //   'line-opacity': 0.9,
+                  // 'line-dasharray': [3, 3],
+                }}
+              />
+            </Source>
+          ))}
       </Map>
     </div>
   );
